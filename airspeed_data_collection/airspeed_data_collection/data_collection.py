6e953f7b258d5732b0b1839b10c4e5ecc8c99b9f
airs-eai <airship@cuhk.edu.cn>
2025-05-12 11:23:25 +0800

    add

--- airspeed_data_collection/airspeed_data_collection/data_collection.py
+++ airspeed_data_collection/airspeed_data_collection/data_collection.py
@@ -28,7 +28,9 @@ class AirspeedDataCollection(Node):
         self.config = self.declare_and_get_parameter('config', 'config/config.yaml').value
         self.DCS_ip = self.config.get('DCS_ip')
         self.DCS_port = self.config.get('DCS_port')
+        self.Data_save_type = self.config.get('Data_save_type')
         self.sock = self.build_connection(self.DCS_ip, self.DCS_port)
+        self.hdf5_path = self.config.get('hdf5_path')
 
         # Initialize utilities
         self.bridge = CvBridge()
@@ -42,8 +44,11 @@ class AirspeedDataCollection(Node):
             callback_group=MutuallyExclusiveCallbackGroup()
         )
 
-        # Send the dataset configuration to the Data Construction Service
-        self.send_settings()
+        if self.Data_save_type == '0':
+            # Send the dataset configuration to the Data Construction Service
+            self.send_settings()
+        elif self.Data_save_type == '1':
+            self.initialize_hdf5(self.hdf5_path, self.data_settings_path)
 
     def declare_and_get_parameter(self, name, default_value):
         """
@@ -154,44 +159,82 @@ class AirspeedDataCollection(Node):
         Returns:
             response: The response object containing the result of the processing or an error message.
         """
-        try:
-            flag = request.flag
-            if flag == "end":
-                self.send_data("end", "end", is_json=False)
-                response.response_message = "Succeed!"
-                return response
-
-            frame_data = request.frame_data
-            rgb_image_msg = request.rgb_image
-            depth_image_msg = request.depth_image
-
-            if not frame_data or not rgb_image_msg or not depth_image_msg:
-                self.get_logger().error("Missing one or more required data fields.")
-                response.response_message = "Fail!"
-                return response
-            else:
-                self.get_logger().info(f"Data frame: {flag} received.")
-
-            self.send_data(frame_data, "frame", is_json=True)
-
+        if self.save_static_data = "0":
             try:
-                cv_rgb_image = self.bridge.imgmsg_to_cv2(rgb_image_msg, desired_encoding='bgr8')
-                cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image_msg, desired_encoding='passthrough')
-                compressed_rgb = self.bridge.cv2_to_compressed_imgmsg(cv_rgb_image, dst_format='jpg').data
-                compressed_depth = self.bridge.cv2_to_compressed_imgmsg(cv_depth_image, dst_format='jpg').data
+                flag = request.flag
+                if flag == "end":
+                    self.send_data("end", "end", is_json=False)
+                    response.response_message = "Succeed!"
+                    return response
 
-                self.send_data(copy.deepcopy(compressed_rgb), "RGB_image", is_json=False)
-                self.send_data(copy.deepcopy(compressed_depth), "depth_image", is_json=False)
-                self.get_logger().info(f"Data frame: {flag} sent successfully!")
-                response.response_message = "Succeed!"
+                frame_data = request.frame_data
+                rgb_image_msg = request.rgb_image
+                depth_image_msg = request.depth_image
+
+                if not frame_data or not rgb_image_msg or not depth_image_msg:
+                    self.get_logger().error("Missing one or more required data fields.")
+                    response.response_message = "Fail!"
+                    return response
+                else:
+                    self.get_logger().info(f"Data frame: {flag} received.")
+
+                self.send_data(frame_data, "frame", is_json=True)
+
+                try:
+                    cv_rgb_image = self.bridge.imgmsg_to_cv2(rgb_image_msg, desired_encoding='bgr8')
+                    cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image_msg, desired_encoding='passthrough')
+                    compressed_rgb = self.bridge.cv2_to_compressed_imgmsg(cv_rgb_image, dst_format='jpg').data
+                    compressed_depth = self.bridge.cv2_to_compressed_imgmsg(cv_depth_image, dst_format='jpg').data
+
+                    self.send_data(copy.deepcopy(compressed_rgb), "RGB_image", is_json=False)
+                    self.send_data(copy.deepcopy(compressed_depth), "depth_image", is_json=False)
+                    self.get_logger().info(f"Data frame: {flag} sent successfully!")
+                    response.response_message = "Succeed!"
+                except Exception as e:
+                    self.get_logger().error(f"Failed to send data: {e}")
+                    response.response_message = "Fail!"
+                return response 
             except Exception as e:
-                self.get_logger().error(f"Failed to send data: {e}")
+                self.get_logger().error(f"Failed to process request: {e}")
                 response.response_message = "Fail!"
-            return response 
-        except Exception as e:
-            self.get_logger().error(f"Failed to process request: {e}")
-            response.response_message = "Fail!"
-            return response
+                return response
+        elif self.save_static_data = "1":
+            try:
+                flag = request.flag
+                if flag == "end":
+                    return "end"
+
+                frame_data = request.frame_data
+                rgb_image_msg = request.rgb_image
+                depth_image_msg = request.depth_image
+
+                if not frame_data or not rgb_image_msg or not depth_image_msg:
+                    self.get_logger().error("Missing one or more required data fields.")
+                    return "Fail"
+                else:
+                    self.get_logger().info(f"Data frame: {flag} received.")
+
+
+                try:
+                    cv_rgb_image = self.bridge.imgmsg_to_cv2(rgb_image_msg, desired_encoding='bgr8')
+                    cv_depth_image = self.bridge.imgmsg_to_cv2(depth_image_msg, desired_encoding='passthrough')
+                    compressed_rgb = self.bridge.cv2_to_compressed_imgmsg(cv_rgb_image, dst_format='jpg').data
+                    compressed_depth = self.bridge.cv2_to_compressed_imgmsg(cv_depth_image, dst_format='jpg').data
+
+                    joint_motion_data = frame_data["execution_data"]["motion_execution_data"]["joint_motion_data"]
+                    gripper_status = frame_data["execution_data"]["motion_execution_data"]["gripper_status"]
+                    timestamp = frame_data["timestamp"]
+
+                    self.append_real_time_data(self.hdf5_path, timestamp, joint_motion_data, gripper_status, compressed_rgb, compressed_depth)
+
+                    self.get_logger().info(f"Data frame: {flag} save successfully!")
+                except Exception as e:
+                    self.get_logger().error(f"Failed to save data: {e}")
+                return "Sucess" 
+            except Exception as e:
+                self.get_logger().error(f"Failed to process request: {e}")
+                return "Fail"
+
 
     def instruct_callback(self, request, response):
         """
@@ -212,6 +255,69 @@ class AirspeedDataCollection(Node):
             self.get_logger().error(f"Exception occurred while processing request: {e}")
             response.response_message = "Fail!"
             return response
+    def initialize_hdf5(self, hdf5_path, data_path):
+        """
+        初始化 HDF5 文件，存储 JSON 元数据，并创建用于存储实时数据的组
+        """
+        output_dir = os.path.dirname(hdf5_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            print(f" 目录 {output_dir} 已创建")
+        
+        try:
+            with open(data_path, 'r') as file:
+                data = json.load(file)
+        except Exception as e:
+            self.get_logger().error(f"Failed to load data: {e}")
+            return
+
+        with h5py.File(hdf5_path, "w") as f:
+            def save_static_data(group, data):
+                """递归存储 JSON 数据"""
+                for key, value in data.items():
+                    if isinstance(value, dict):
+                        subgroup = group.create_group(key)  # 创建字典对应的子组
+                        save_static_data(subgroup, value)
+                    elif isinstance(value, list):
+                        subgroup = group.create_group(key)  # 处理列表，存为 0,1,2...
+                        for i, item in enumerate(value):
+                            sub_group = subgroup.create_group(str(i))
+                            save_static_data(sub_group, item)
+                    else:
+                        group.attrs[key] = value if isinstance(value, str) else str(value)
+
+        #  存储属性
+        root_group = f.create_group("robot_motion_sample")
+        save_static_data(root_group, data["robot_motion_sample"])
+
+        # 创建存储实时数据的组
+        f.create_group("dataset/execution_data/motion_execution_data/joint_motion_data")
+        f.create_group("dataset/execution_data/motion_execution_data/gripper_status")
+        f.create_group("dataset/execution_data/perception_data/rgb_image")
+        f.create_group("dataset/execution_data/perception_data/depth_image")
+
+        print(f" HDF5 文件已初始化: {hdf5_path}")
+    
+    def append_real_time_data(self, hdf5_path, timestamp, joint_motion_data, gripper_status, rgb_image, depth_image):
+        """
+        追加实时数据到 HDF5
+        """
+        with h5py.File(hdf5_path, "a") as f:  #  （追加数据）
+            # 追加关节运动数据
+            jm_group = f["dataset/execution_data/motion_execution_data/joint_motion_data"]
+            jm_group.create_dataset(timestamp, data=joint_motion_data)
+
+            #  追加夹爪状态
+            gs_group = f["dataset/execution_data/motion_execution_data/gripper_status"]
+            gs_group.create_dataset(timestamp, data=gripper_status)
+
+            #  追加 RGB 图像
+            rgb_group = f["dataset/execution_data/perception_data/rgb_image"]
+            rgb_group.create_dataset(timestamp, data=rgb_image)
+
+            # 追加深度图像
+            depth_group = f["dataset/execution_data/perception_data/depth_image"]
+            depth_group.create_dataset(timestamp, data=depth_image)
 
 def main(args=None):
     rclpy.init(args=args)
