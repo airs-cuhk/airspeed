38279e0187ca029d137ecf582811a4f1a86ab322
StarChen-Cycler <1976693250@qq.com>
2026-06-03 21:16:35 +0800

    chore: ignore tests folder

--- src/data_collection_service/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-ROOT = Path(__file__).resolve().parents[1]
-SRC = ROOT / "src"
-TOOLS = ROOT / "tools"
-for p in (str(SRC), str(TOOLS)):
-    if p not in sys.path:
-        sys.path.insert(0, p)
